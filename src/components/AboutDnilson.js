8999c1d28ebbd154abcf68630e8cd619efab8dc4
Dnilson Achahuanco Huarilloclla <dnilson.achahuanco@tecsup.edu.pe>
2025-06-16 10:20:49 -0500

    lAB COMPLET

---
+++ src/components/AboutDnilson.js
@@ -0,0 +1,14 @@
+// src/components/AboutDnilson.js
+import React from 'react';
+
+function AboutDnilson() {
+    return (
+        <div>
+            <p>Hola, soy Dnilson.</p>
+            <p>Carrera: Diseño y desarrollo de software</p>
+            <p>Me gusta el voley y Python &lt;3</p>
+        </div>
+    );
+}
+
+export default AboutDnilson;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,38 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
+import dynamic from "next/dynamic";
+
+
+const DynamicAboutDnilson = dynamic(() => import("../components/AboutDnilson"), {
+  ssr: false, // This ensures the component is only rendered on the client side
+});
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Mi Sitio Optimizado - Home</title>
-        <meta name="description" content="Aprende sobre optimización SEO y rendimiento en Next.js." />
-        <meta name="keywords" content="Next.js, SEO, optimización web" />
-        <meta property="og:title" content="Mi Sitio Optimizado" />
-        <meta property="og:description" content="Descubre técnicas avanzadas para mejorar tu web con Next.js." />
-        <meta property="og:image" content="/images/seo-image.png" />
-        <meta property="og:type" content="website" />
-      </Head>
-
-      <h1>Bienvenido a mi página optimizada</h1>
-      <p>Aprende cómo mejorar el rendimiento y SEO en Next.js.</p>
+      <h1>Bienvenido</h1>
+      <DynamicAboutDnilson />
     </>
   );
 }
+
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Head>
+//         <title>Mi Sitio Optimizado - Home</title>
+//         <meta name="description" content="Aprende sobre optimización SEO y rendimiento en Next.js." />
+//         <meta name="keywords" content="Next.js, SEO, optimización web" />
+//         <meta property="og:title" content="Mi Sitio Optimizado" />
+//         <meta property="og:description" content="Descubre técnicas avanzadas para mejorar tu web con Next.js." />
+//         <meta property="og:image" content="/images/seo-image.png" />
+//         <meta property="og:type" content="website" />
+//       </Head>
+
+//       <h1>Bienvenido a mi página optimizada</h1>
+//       <p>Aprende cómo mejorar el rendimiento y SEO en Next.js.</p>
+//     </>
+//   );
+// }
